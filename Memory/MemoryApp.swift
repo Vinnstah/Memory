05a25ad8f82b1fcbbddf2dccd3db3ced6f8c9a91
Viktor Jansson <v.l.jansson@gmail.com>
2022-03-01 16:50:59 +0100

    Initial commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Viktor Jansson on 2022-03-01.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
